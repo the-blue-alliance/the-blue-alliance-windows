da187a0301f529cf56bd0faec9947d8f46b61a45
David Thomas <dev@synth3.tk>
2016-05-16 01:28:19 -0400

    Add season selector to event listing page

--- TheBlueAlliance/ViewModels/EventListViewModel.cs
+++ TheBlueAlliance/ViewModels/EventListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using DynamicData;
 using DynamicData.Binding;
 using System.Collections.ObjectModel;
@@ -11,21 +12,44 @@ namespace TBA.ViewModels
     public class EventListViewModel : AbstractNotifyPropertyChanged
     {
         private readonly ReadOnlyObservableCollection<EventModel> _data;
+        private SourceList<EventModel> eventList = new SourceList<EventModel>();
+        private int _selectedSeason;
 
         public EventListViewModel()
         {
-            var eventList = new SourceList<EventModel>();
+            SelectedSeason = Constants.MaxSeason();
+
+            var filter = this.WhenValueChanged(s => s.SelectedSeason)
+                .Select(BuildFilter);
+
             var eventQuery = DataStoreHelper.db.Table<EventModel>();
+
             foreach (var eventResult in eventQuery)
                 eventList.Add<EventModel>(eventResult);
+
             var eventObservables = eventList.Connect()
+                .Filter(filter)
                 .Bind(out _data)
                 .Subscribe();
+        }
+
+        private static Func<EventModel, bool> BuildFilter(int SelectedSeason)
+        {
+            return e => e.Year.Equals(SelectedSeason);
         }
 
         public ReadOnlyObservableCollection<EventModel> Data
         {
             get { return _data; }
+        }
+
+        public int SelectedSeason {
+            get { return _selectedSeason; }
+            set
+            {
+                _selectedSeason = value;
+                OnPropertyChanged("SelectedSeason");
+            }
         }
     }
 }
--- TheBlueAlliance/Views/Events/EventListing.xaml.cs
+++ TheBlueAlliance/Views/Events/EventListing.xaml.cs
@@ -1,5 +1,6 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System;
 using TBA.Models;
 using TBA.ViewModels;
 
@@ -10,19 +11,37 @@ namespace TBA.Views.Events
     /// </summary>
     public sealed partial class EventListing : Page
     {
+        public EventListViewModel Events { get; set; }
+
         public EventListing()
         {
             InitializeComponent();
             this.NavigationCacheMode = NavigationCacheMode.Required;
-            EventListViewModel events = new EventListViewModel();
-            EventList.ItemsSource = events.Data;
+            Events = new EventListViewModel();
+            EventList.ItemsSource = Events.Data;
+            SeasonSelector.ItemsSource = Constants.SeasonList();
+            SeasonSelector.SelectedItem = Events.SelectedSeason;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (e.Parameter != null)
+            {
+                Events.SelectedSeason = Convert.ToInt32(e.Parameter);
+            }
         }
 
         private void EventList_ItemClick(object sender, ItemClickEventArgs e)
         {
             var item = e.ClickedItem as EventModel;
-            //string k = enter.Text;
             this.Frame.Navigate(typeof(EventDetail), item.Key.ToString());
+        }
+
+        private void SeasonSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Events.SelectedSeason = Convert.ToInt32(SeasonSelector.SelectedItem);
         }
     }
 }
